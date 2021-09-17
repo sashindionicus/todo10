d7f4c537b6c6c41721fc0b79f8378d4aafb0e300
Your Name <you@example.com>
2021-09-17 19:41:41 +0300

    second

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,33 +4,58 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
-  constructor(){
+  constructor() {
     super()
     // создаем конструктор для добавления ивент листенера в реакт
     this.onLabelClick = () => {
-      console.log(`Done: ${this.props.label}`)
+      this.setState((state) => {
+        return {
+          done: !state.done
+        }
+      })
+    }
+
+    this.onMarkImportant = () => {
+      this.setState((state) => {
+        return {
+          important: !state.important
+        }
+      })
+    }
+
+    this.state = {
+      done: false,
+      important: false      
     }
   }
 
-  render() {
-    const { label, important = false } = this.props
+  
 
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
+  render() {
+    const { label } = this.props
+    // деструктуризация свойств, чтобы достать их из состояния
+    const { done, important } = this.state
+
+    let classNames = 'todo-list-item'
+    if(done) {
+      classNames += ' done'
+    }
+
+    if (important) {
+      classNames += ' important'
+    }
   
     return (
-      <span className="todo-list-item">
+      <span className={classNames}>
         <span
           className="todo-list-item-label"
-          style={style}
           onClick={ this.onLabelClick }>
           {label}
         </span>
   
         <button type="button"
-                className="btn btn-outline-success btn-sm float-right">
+                className="btn btn-outline-success btn-sm float-right"
+                onClick={this.onMarkImportant}>
           <i className="fa fa-exclamation" />
         </button>
   
